34cb37a4b4de2e7eb9b82e224708ec9f1109e976
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-03-07 21:14:06 -0500

    add geoLocation api to acquire user's location

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React, { useState, useEffect } from "react";
 import Header from "./Components/Header/header";
 import Box from "./Components/Posts/box";
 import "./App.scss";
 import Footer from "./Components/Footer/Footer";
 
 function App() {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+      console.log(latitude);
+      console.log(longitude);
+    });
+  }, []);
+
   return (
     <>
       <Header />
-      <Box />
+      <Box longitude={longitude} latitude={latitude} />
       <Footer />
     </>
   );
--- src/Components/Posts/box.js
+++ src/Components/Posts/box.js
@@ -2,15 +2,18 @@ import React from "react";
 import "./box.scss";
 import axios from "axios";
 export default function Box(props) {
-  axios
-    .get(
-      "https://www.hadithapi.com/api/hadiths?apiKey=$2y$10$ibAptVE2DF5023ErmcjcOoJH5G66yLL1a0sYwonI20s1g6FwT0u"
-    )
-    .then((res) => console.log(res.data.hadiths));
+  // axios
+  //   .get(
+  //     "https://www.hadithapi.com/api/hadiths?apiKey=$2y$10$ibAptVE2DF5023ErmcjcOoJH5G66yLL1a0sYwonI20s1g6FwT0u"
+  //   )
+  //   .then((res) => console.log(res));
 
   return (
     <article>
       <section className="article-container">{}</section>
+      <h1>
+        Longitude: {props.longitude} Latitude: {props.latitude}
+      </h1>
     </article>
   );
 }
