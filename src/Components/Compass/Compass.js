7f62c7e403e9b5badefc5a7cdcbf37bcc4bbdade
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-03-22 01:39:57 -0400

    clean up code and improve compass style to match theme

--- src/Components/Compass/Compass.js
+++ src/Components/Compass/Compass.js
@@ -7,43 +7,6 @@ export default function Compass() {
   const [location, setLocation] = useState({ longitude: null, latitude: null });
   const [angle, setAngle] = useState(0);
 
-  // useEffect(() => {
-  // const fetchData = async () => {
-  //   try {
-  //     const res = await axios.get(
-  //       `http://api.aladhan.com/v1/qibla/${location.latitude}/${location.longitude}`
-  //     );
-  //     console.log(res.data);
-  //   } catch (err) {
-  //     console.log("ERROR:", err);
-  //   }
-  // };
-
-  //     const getLocation = () => {
-  //       return new Promise((resolve, reject) => {
-  //         navigator.geolocation.getCurrentPosition(resolve, reject);
-  //       });
-  //     };
-
-  //     const fetchDataWithLocation = async () => {
-  //       try {
-  //         const position = await getLocation();
-  //         const { latitude, longitude } = position.coords;
-  //         setLocation({ longitude, latitude });
-  //         console.log("LOCATION:", latitude, longitude);
-  //       } catch (error) {
-  //         console.log("Error getting location:", error);
-  //       }
-  //     };
-  //     if (location.latitude === null && location.longitude === null) {
-  //       fetchDataWithLocation();
-  //     } else {
-  //       fetchData();
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -83,22 +46,25 @@ export default function Compass() {
     }
   }, [location]);
 
-  // window.addEventListener("deviceorientation", handleOrientation);
-  // return () => {
-  //   window.removeEventListener("deviceorientation", handleOrientation);
-
   return (
     <div className="compass-div">
       <svg viewBox="0 0 100 100" className="compass">
         <circle cx="50" cy="50" r="45" className="compass-background" />
+
         <line
           x1="50"
           y1="50"
           x2="50"
-          y2="10"
+          y2="30"
           className="compass-indicator"
           style={{ transform: `rotate(${angle}deg)` }}
         />
+        <polygon
+          points="48,30 50,25 52,30"
+          className="compass-indicator"
+          style={{ transform: `rotate(${angle}deg)` }}
+        />
+
         <text x="50" y="15" textAnchor="middle" className="compass-north">
           N
         </text>
