2afff0f3f1e850e54553aa2081063cae3dbbcdb7
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-03-26 09:46:29 -0400

    implement a condition to hide the footer so it doesn't block page content when it's too long

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import React, { useState, useEffect } from "react";
 import Header from "./Components/Header/header";
-import Box from "./Components/Posts/box";
 import "./App.scss";
 import Footer from "./Components/Footer/Footer";
 import PrayerTable from "./Components/PrayerTable";
 import Background from "./Background";
 import Compass from "./Components/Compass/Compass";
+import HadithBox from "./Components/HadithBox/box";
 
 function App() {
+  const [showFooter, setShowFooter] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  function handleScroll() {
+    if (window.scrollY > 0 && showFooter) {
+      setShowFooter(false);
+    } else if (window.scrollY === 0 && !showFooter) {
+      setShowFooter(true);
+    }
+  }
+
   return (
     <div className="components">
       <div className="prayer-comp"></div>
@@ -15,7 +33,9 @@ function App() {
         <Background />
       </div>
       <PrayerTable />
-      <Footer />
+      <HadithBox />
+
+      {showFooter && <Footer />}
     </div>
   );
 }
