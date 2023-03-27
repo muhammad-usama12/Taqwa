aef0de479f40676746af5c70631936e9df19afaa
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-03-27 08:32:27 -0400

    reorganize Label component and add glow effect

---
+++ src/Components/Label/label.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Heading, Mark } from "@chakra-ui/react";
+import "./label.scss";
+
+export default function Label(props) {
+  return (
+    <Heading lineHeight="tall" zIndex={999}>
+      <Mark
+        bg={"whiteAlpha.900"}
+        fontFamily={""}
+        px={2}
+        py={1}
+        query={props.first}
+      >
+        {props.first}
+      </Mark>
+      <Mark
+        color={"whiteAlpha.900"}
+        // bg={"whiteAlpha.900"}
+        fontFamily={""}
+        px={2}
+        py={1}
+        query={props.last}
+      >
+        {props.last}
+      </Mark>
+    </Heading>
+  );
+}
